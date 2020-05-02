73f3fffea61fe682bb37df7a0c914a1bc1c7be12
sesamestrong <doolie.healey@gmail.com>
2020-05-02 00:33:12 -0400

    First prototype

---
+++ backend/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const request = require("request-promise");
+require("dotenv").config();
+const app = express();
+app.use(require("cors")());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+(async () => {
+    const {
+        Game
+    } = require("./mongoose.js");
+    app.post("/challenge", async (req, res) => {
+        const {
+            query,
+            endTerm,
+            allowAnswer
+        } = req.body;
+        const game = await Game.new(query, endTerm, allowAnswer);
+        res.json({
+            id: game._id,
+            query: game.query,
+            questions: game.questions,
+            endTerm: game.endTerm,
+            allowAnswer: game.allowAnswer,
+            complete: game.completed
+        });
+    });
+    app.get("/click",async (req, res)=>{
+        const {idx,id}=req.query;
+        if(!(idx&&id)) return res.status(400).json({err:"Required fields missing."});
+        const game=await Game.findById(id);
+        if(!game) return res.status(404).json({err:"Game not found"});
+        await game.click(idx);
+        res.json({
+            id: game._id,
+            query: game.query,
+            questions: game.questions,
+            endTerm: game.endTerm,
+            allowAnswer: game.allowAnswer,
+            complete: game.completed
+        });
+    });
+    app.get("/challenge", async (req, res) => {
+        const {id}=req.query;
+        if(!id) return res.status(400).json({err:"Missing required field."});
+        const existing = await Game.findById(id);
+        res.json({
+            id: game._id,
+            query: game.query,
+            questions: game.questions,
+            endTerm: game.endTerm,
+            allowAnswer: game.allowAnswer,
+            complete: game.completed
+        });
+    });
+    app.get("/google", async (req, res) => {const txt=await request({uri:"https://www.google.com/search","qs":{q:"irish potato famine"}});res.send(txt);});
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log("Server listening on port " + port));
+})();
---
+++ backend/mongoose.js
@@ -0,0 +1,196 @@
+require("dotenv").config();
+const {
+    runEntireScraper
+} = require("@sesamestrong/json-scraper");
+const bcrypt = require("bcrypt");
+const NUM_ROUNDS = 12;
+const startGame = require("./getPaaInfo.json");
+const getNewQuestions = require("./getNewQuestions.json");
+
+const mongoose = require("mongoose");
+const dbUrl = process.env.DATABASE.replace(/<password>/, process.env.PASSWORD).replace(/<division>/, process.env.DIVISION);
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const {
+    ObjectId
+} = mongoose.Schema.Types;
+
+// Start schemas
+
+let users, User, games, Game;
+
+games = new mongoose.Schema({
+    query: {
+        type: String,
+        required: true
+    },
+    endTerm: {
+        type: String,
+        required: true
+    },
+    numClicks: {
+        type: Number,
+        required: true
+    },
+    questions: [{
+        question: {
+            type: String,
+            required: true
+        },
+        answer: {
+            type: String,
+            required: true
+        },
+        kt: {
+            type: String,
+            required: true
+        },
+    }],
+    completed: {
+        type: Boolean,
+        required: true
+    },
+    ved: {
+        type: String,
+        required: false
+    },
+    id: {
+        type: String,
+        required: false
+    },
+    cs: {
+        type: String,
+        required: true
+    },
+    ei: {
+        type: String,
+        required: true
+    },
+    allowAnswer: {
+        type: Boolean,
+        required: true
+    },
+});
+
+games.statics.new = async function(query, endTerm, allowAnswer) {
+    const {
+        questions: rawQuestions,
+        answers,
+        kts,
+        id,
+        cs,
+        ved,
+        ei,
+        info
+    } = (await runEntireScraper(startGame, {
+        vars: {
+            query
+        }
+    })).vars;
+    console.log(info);
+    const game = await new Game({
+        query,
+        endTerm,
+        numClicks: 0,
+        questions: rawQuestions.map((rawQuestion, idx) => ({
+            question: rawQuestion,
+            answer: answers[idx],
+            kts: kts[idx]
+        })),
+        ei,
+        id,
+        cs,
+        ved,
+        completed: false,
+        allowAnswer
+    });
+    if (game.hasWon()) game.completed = true;
+    await game.save();
+    return game;
+};
+games.methods.click = async function(idx) {
+    if (this.completed) return;
+    const question = this.questions[idx];
+    const {
+        questions,
+        answers,
+        kts
+    } = (await runEntireScraper(getNewQuestions, {
+        vars: {
+            id: this.id,
+            ei: this.ei,
+            ved: this.ved,
+            kt: question.kt,
+            query: question.question,
+            cs: this.cs
+        }
+    })).vars.questions;
+    const newQuestions = questions.map((question, idx) => ({
+        question,
+        kt: kts[ids],
+        answer: answers[idx]
+    }));
+    this.questions = [...this.questions, ...newQuestions];
+    this.numClicks++;
+    if (this.hasWon(newQuestions)) this.completed = true;
+    await this.save();
+};
+games.methods.hasWon = function(questions = this.questions) {
+    const lowKey = this.endTerm.toLowerCase();
+    return questions.find(question => question.question.toLowerCase().includes(lowKey) || this.allowAnswer && question.answer.toLowerCase().includes(lowKey));
+};
+Game = mongoose.model("Game", games);
+/*
+accounts = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: String,
+    email: String,
+    gameIds: [{
+        type: ObjectId,
+        required: true,
+        unique: true,
+        ref: 'Game'
+    }]
+});
+
+accounts.statics.new = async function({
+    username,
+    password,
+    name
+}) {
+    const matches = await Account.findOne({
+        username
+    });
+    if (matches) throw new Error('Account already exists');
+    const account = new Account({
+        username,
+        name,
+        password: await bcrypt.hash(password, NUM_ROUNDS)
+    });
+    await account.save();
+    return account;
+};
+
+accounts.statics.auth = async function({username,password}){
+    const matches=await Account.findOne({username});
+    if(!matches) return false;
+    if(!await bcrypt.compare(password,matches.password)) return false;
+    return true;
+};
+*/
+
+module.exports = {
+    Game
+};
