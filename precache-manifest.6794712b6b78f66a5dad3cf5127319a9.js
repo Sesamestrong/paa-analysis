c554ebe208be6e156a2869ee4f607c476729ba62
Sesamestrong <Sesamestrong@users.noreply.github.com>
2020-05-19 20:45:16 +0000

    deploy: 05c034269cfb956fea1f6e34a435b89bd9af17a6

---
+++ precache-manifest.6794712b6b78f66a5dad3cf5127319a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8196da19dd0b3dfb1dc25bb775f956",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dda554ece47c4ee2154a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "241f6dc2f7c4256435dd",
+    "url": "/static/js/2.871f0e09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.871f0e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda554ece47c4ee2154a",
+    "url": "/static/js/main.27f1fb95.chunk.js"
+  },
+  {
+    "revision": "0c6ffd730469f9946028",
+    "url": "/static/js/runtime-main.f4d1916d.js"
+  }
+]);
