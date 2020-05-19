2a8ce703f57056021eac44bcd3828fd87525d488
Sesamestrong <doolie.healey@gmail.com>
2020-05-19 04:19:02 -0400

    Working product yay!

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const request = require("request-promise");
+const UserAgent = require("user-agents");
 require("dotenv").config();
 const app = express();
 app.use(require("cors")());
@@ -31,11 +32,19 @@ app.use(bodyParser.urlencoded({
             complete: game.completed
         });
     });
-    app.get("/click",async (req, res)=>{
-        const {idx,id}=req.query;
-        if(!(idx&&id)) return res.status(400).json({err:"Required fields missing."});
-        const game=await Game.findById(id);
-        if(!game) return res.status(404).json({err:"Game not found"});
+    app.get("/click", async (req, res) => {
+        const {
+            idx,
+            id
+        } = req.query;
+        console.log(idx,id);
+        if (!(idx && id!==undefined)) return res.status(400).json({
+            err: "Required fields missing."
+        });
+        const game = await Game.findById(id);
+        if (!game) return res.status(404).json({
+            err: "Game not found"
+        });
         await game.click(idx);
         res.json({
             id: game._id,
@@ -47,9 +56,13 @@ app.use(bodyParser.urlencoded({
         });
     });
     app.get("/challenge", async (req, res) => {
-        const {id}=req.query;
-        if(!id) return res.status(400).json({err:"Missing required field."});
-        const existing = await Game.findById(id);
+        const {
+            id
+        } = req.query;
+        if (!id) return res.status(400).json({
+            err: "Missing required field."
+        });
+        const game  = await Game.findById(id);
         res.json({
             id: game._id,
             query: game.query,
@@ -59,7 +72,16 @@ app.use(bodyParser.urlencoded({
             complete: game.completed
         });
     });
-    app.get("/google", async (req, res) => {const txt=await request({uri:"https://www.google.com/search","qs":{q:"irish potato famine"}});res.send(txt);});
+    app.get("/google", async (req, res) => {
+        const txt = await request({
+            uri: "https://www.google.com/search",
+            "qs": {
+                q: "irish potato famine"
+            },
+            headers:{"User-Agent": (new UserAgent()).toString()}
+        });
+        res.send(txt);
+    });
 
     const port = process.env.PORT || 3000;
     app.listen(port, () => console.log("Server listening on port " + port));
